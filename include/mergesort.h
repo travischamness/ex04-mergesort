852948734082158ef887139eb2ab092c4ae11486
travischamness <travis_chamness1@my.vcccd.edu>
2018-11-09 11:36:31 -0800

    11-9 More updating

--- include/mergesort.h
+++ include/mergesort.h
@@ -5,63 +5,71 @@
 #ifndef EX04_MERGESORT_MERGESORT_H
 #define EX04_MERGESORT_MERGESORT_H
 #include <cstdint>
+using std::size_t;
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
-    template<typename T>
-    void mergeSort(T array[], size_t size) {
-        T j = (i + k)/2; //location identities within array
-        T i = 0;
-        T k = sizeof(array) - 1;
-
-        while( right > 1eft ){
-            mergeSort(T array[], int left, int middle);
-            mergeSort(T array[], int middle + 1, int right);
-        }
-
-    }
-    template<typename T>
-    void merge(T array[], T i, T j, T k){
-        T left = i; //T to keep everything of same type
-        T right = j + 1;
-        T middle = j;
-
-        newSize = k - i + 1; //Details the full size of the new array
-        mergeIndex = 0; //Initialized index of merged element
-        mergedNumbers = new int[mergeIndex];
-
-        while(left <= j && right <= k){
-            if(array[left] <= array[right]){//organizing new heap array in order from least to greatest
-                mergedNumbers[mergedIndex] = array[left];
-                left++;
-            }
-            else{
-                mergedNumbers[mergeIndex] = array[right];
-                right++;
-            }
-            mergeIndex++; //increments up, sorting in each lesser element of the iteration. GENIUS
-        }
-
-        while(left <= j){//when loop above ends, need to include remaining values if any exist
-            //Case where left set is larger than right set or singular
-            mergedNumbers[mergeIndex] = array[left];
-            left++;
-            mergeIndex++
-        }
-        while(right <= k){//Case where right set is larger than left set or singular
-            mergedNumbers[mergeIndex] = array[right];
-            right++;
-            mergeIndex++;
-        }
-        for(mergeIndex = 0; mergeIndex <= k-1; mergeIndex++){ //Copying the sorted indecies in the temporary array
-            array[mergeIndex] = mergedNumbers[mergeIndex];
-        }
-
-    }
+                template<typename T>
+                void mergesort(T array[], size_t size) {
+                    T _start = 0;
+                    T _end = size - 1 ;
 
 
+                    void _mergesort(array, _start, _end){
+                        T newStart = _start;
+                        T newEnd =  _end ;
+                        T half = (newStart + newEnd)/2;
+
+                        while ( newEnd > newStart ){
+                            _mergesort(array, newStart, half);
+                            _mergesort(array, half + 1, newEnd);
+                            merge(array, newStart, half, newEnd);
+                        }
+                    }
+
+                }
+                template<typename T>
+                void merge(T array[], T i, T j, T k){
+                    T L = i; //starter index of Left side
+                    T R = j + 1; //starter index of Right side
+                    T middle = j;
+
+                    T newSize = R - L + 1; //Details the full size of the new array
+                    T mergeIndex = 0; //Initialized index of merged element
+                    T mergedNumbers = new int[mergeIndex]; //iterator position 0
+
+                    while(L <= j && R <= k){
+                        if(array[L] <= array[R]){//organizing new heap array in order from least to greatest
+                            mergedNumbers[mergeIndex] = array[L];
+                            L++;
+                        }
+                        else{
+                            mergedNumbers[mergeIndex] = array[R];
+                            R++;
+                        }
+                        mergeIndex++; //increments up, sorting in each lesser element of the iteration. GENIUS
+                    }
+
+                    while(L <= j){//when loop above ends, need to include remaining values if any exist
+                        //Case where left set is larger than right set or singular
+                        mergedNumbers[mergeIndex] = array[L];
+                        L++;
+                        mergeIndex++;
+                    }
+                    while(R <= k){//Case where right set is larger than left set or singular
+                        mergedNumbers[mergeIndex] = array[R];
+                        R++;
+                        mergeIndex++;
+                    }
+                    for(mergeIndex = 0; mergeIndex <= k-1; mergeIndex++){ //Copying the sorted indecies in the temporary array
+                        array[mergeIndex] = mergedNumbers[mergeIndex];
+                    }
+
+                }
 
 
-}}}}
+//                delete [] mergeIndex;
 
-#endif //EX04_MERGESORT_MERGESORT_H
+            }}}}
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -12,9 +12,14 @@
 #include"mergesort.h"
 #include<iostream>
 #include<string>
+using std::endl;
+using std::cout;
 
 int main(){
 
     int64_t array[] = {1,3,2,7,4,10};
-    void partition(array,0,5);
+    int64_t size = 6;
+    edu::vcccd::vc::csv15::mergesort(array, 6);
+
+
 }
