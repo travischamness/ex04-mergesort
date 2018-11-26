abfa0848bf0a57eaf1ea7f0b1b6b981c050f212c
travischamness <travis_chamness1@my.vcccd.edu>
2018-11-26 14:52:56 -0800

    11-26 Cleaning

--- include/mergesort.h
+++ include/mergesort.h
@@ -10,65 +10,65 @@ using std::size_t;
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
                 template<typename T>
-                void mergesort(T array[], size_t size) {
-                    T _start = 0;
-                    T _end = size - 1 ;
+                void merge(T array[], T _start, T halfpoint, T _end){
+                    uint64_t L = _start; //starter index of Left side
+                    uint64_t R = halfpoint + 1; //starter index of Right side
 
 
-                    void _mergesort(array, _start, _end){
-                        T newStart = _start;
-                        T newEnd =  _end ;
-                        T half = (newStart + newEnd)/2;
+                    T newSize = (R - L) + 1; //Details the full size of the new array
+                    int mergeIndex = 0; //Initialized index of merged element
+                    T mergedValues[] = new int [mergeIndex]; //iterator position 0
 
-                        while ( newEnd > newStart ){
-                            _mergesort(array, newStart, half);
-                            _mergesort(array, half + 1, newEnd);
-                            merge(array, newStart, half, newEnd);
-                        }
-                    }
-
-                }
-                template<typename T>
-                void merge(T array[], T i, T j, T k){
-                    T L = i; //starter index of Left side
-                    T R = j + 1; //starter index of Right side
-                    T middle = j;
-
-                    T newSize = R - L + 1; //Details the full size of the new array
-                    T mergeIndex = 0; //Initialized index of merged element
-                    T mergedNumbers = new int[mergeIndex]; //iterator position 0
-
-                    while(L <= j && R <= k){
+                    while(L <= halfpoint && R <= _end){
                         if(array[L] <= array[R]){//organizing new heap array in order from least to greatest
-                            mergedNumbers[mergeIndex] = array[L];
+                            mergedValues[mergeIndex] = array[L];
                             L++;
                         }
                         else{
-                            mergedNumbers[mergeIndex] = array[R];
+                            mergedValues[mergeIndex] = array[R];
                             R++;
                         }
                         mergeIndex++; //increments up, sorting in each lesser element of the iteration. GENIUS
                     }
 
-                    while(L <= j){//when loop above ends, need to include remaining values if any exist
+                    while(L <= halfpoint){//when loop above ends, need to include remaining values if any exist
                         //Case where left set is larger than right set or singular
-                        mergedNumbers[mergeIndex] = array[L];
+                        mergedValues[mergeIndex] = array[L];
                         L++;
                         mergeIndex++;
                     }
-                    while(R <= k){//Case where right set is larger than left set or singular
-                        mergedNumbers[mergeIndex] = array[R];
+                    while(R <= _end){//Case where right set is larger than left set
+                        mergedValues[mergeIndex] = array[R];
                         R++;
                         mergeIndex++;
                     }
-                    for(mergeIndex = 0; mergeIndex <= k-1; mergeIndex++){ //Copying the sorted indecies in the temporary array
-                        array[mergeIndex] = mergedNumbers[mergeIndex];
+                    for(mergeIndex = 0; mergeIndex <= _end-1; mergeIndex++){ //Copying the sorted indecies in the temporary array
+                        array[mergeIndex] = mergedValues[mergeIndex];
+                    }
+                }
+
+                template<typename T>
+                void _mergesort(T array[], uint64_t _start, uint64_t _end){
+//                    uint64_t _start = _start;
+//                    uint64_t _end =  _end ;
+                    uint64_t halfpoint = _start + (_start + _end)/2;
+
+                    if ( _end > _start ){
+                        _mergesort(array, _start, halfpoint);
+                        _mergesort(array, halfpoint + 1, _end);
+                        merge(array, _start, halfpoint, _end);
                     }
 
                 }
 
+                template<typename T>
+                void mergesort(T array[], size_t size) {
+                    uint64_t _start = 0;
+                    uint64_t _end = size - 1;
 
-//                delete [] mergeIndex;
+                    _mergesort(array, _start, _end);
+
+                }
 
             }}}}
 
